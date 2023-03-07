2801cef8e0584c1915e19b285007d4834b20c236
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-03-07 15:26:48 +0000

    signup fix

--- db/migrate/20230306142921_devise_create_users.rb
+++ db/migrate/20230306142921_devise_create_users.rb
@@ -4,9 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :username
-      t.string :first_name
-      t.string :last_name
 
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
