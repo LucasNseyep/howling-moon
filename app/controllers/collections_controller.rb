2f5d84582904b380a2b020d77b1b933d3c40a832
Enyo HumphreyAckumey <enyomha@gmail.com>
2023-03-09 18:02:52 +0000

    I can't get the destroy to work, can't remember how to do it

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -8,10 +8,25 @@ class CollectionsController < ApplicationController
     @thoughts = Thought.where(collection_id: @collection.id)
   end
 
+  def edit
+    @collection = Collection.find(params[:id])
+  end
+
  def update
   @collection = Collection.find(params[:id])
   @collection.update(collection_params)
   @collection.save
+  redirect_to collections_path
+ end
+
+ def destroy
+  @collection = Collection.find(params[:id])
+  @collection.thoughts.each do |thought|
+    thought.collection_id = nil
+  end
+  @collection.save
+  # raise
+  @collection.destroy
   redirect_to collections_path
  end
 
