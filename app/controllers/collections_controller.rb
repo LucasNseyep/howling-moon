21c37a587d0cf0c45fae0b39480d04bfbb5022fe
Carolina Bacelar <carolrbacelar@hotmail.com>
2023-03-13 15:46:21 +0000

    add style to the new collection page

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -34,28 +34,41 @@ class CollectionsController < ApplicationController
     @collection = Collection.find(params[:id])
   end
 
- def update
-  @collection = Collection.find(params[:id])
-  @collection.update(collection_params)
-  @collection.save
-  redirect_to collections_path
- end
+  def new
+    @collection = Collection.new
+  end
 
- def destroy
-  @collection = Collection.find(params[:id])
-  # THINK THIS WILL WORK BUT NEED TO GO OVER IT ALL AGAIN TOMORROW
-  # CURRENTLY NOT LIKING THE DESTROY ON THE disconnect METHOD
-  # @collection.thoughts.each do |thought|
-  #   thought.disconnect(thought[:id])
-  #   thought.save
-  # end
-  @collection.destroy
-  redirect_to collections_path
- end
+  def create
+    @collection = Collection.new(collection_params)
+    @collection.user = current_user
+    @collection.save
 
- private
+    redirect_to collections_path
+    # redirect_to collection_path(@collection)
+  end
 
- def collection_params
-  params.require(:collection).permit(:title, :description)
- end
+  def update
+    @collection = Collection.find(params[:id])
+    @collection.update(collection_params)
+    @collection.save
+    redirect_to collections_path
+  end
+
+  def destroy
+    @collection = Collection.find(params[:id])
+    # THINK THIS WILL WORK BUT NEED TO GO OVER IT ALL AGAIN TOMORROW
+    # CURRENTLY NOT LIKING THE DESTROY ON THE disconnect METHOD
+    # @collection.thoughts.each do |thought|
+    #   thought.disconnect(thought[:id])
+    #   thought.save
+    # end
+    @collection.destroy
+    redirect_to collections_path
+  end
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:title, :description)
+  end
 end
