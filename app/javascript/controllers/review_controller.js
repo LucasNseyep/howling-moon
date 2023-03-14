581b30b31babb225ba6cb0a6e2c5e0e1d7669388
arman akther <armanakther@live.co.uk>
2023-03-14 16:04:03 +0000

    Media query complete and new review moon theme

--- app/javascript/controllers/review_controller.js
+++ app/javascript/controllers/review_controller.js
@@ -4,70 +4,36 @@
 
 //   connect() {
 
-// const section4 = document.querySelector(".section4");
-// const reviews = section4.querySelectorAll(".review");
+    // const reviews = document.getElementsByClassName("review");
 
-// let globalIndex = 0;
-// last = { x: 0, y: 0 };
+    // let globalIndex = 0;
+    //   last = { x: 0, y: 0};
 
-// const activate = (review, x, y) => {
-//   review.style.left = `${x}px`;
-//   review.style.top = `${y}px`;
+    // const activate = (review, x, y) => {
+    //   review.style.left = `${x}px`;
+    //   review.style.top = `${y}px`;
 
-//   review.dataset.status = "active";
+    //   review.dataset.status = 'active';
 
-//   last = { x, y };
-// };
+    //   last = { x, y };
+    // }
 
-// const distanceFromLast = (x, y) => {
-//   return Math.hypot(x - last.x, y - last.y);
-// };
+    // const distanceFromLast = (x, y) => {
+    //   return Math.hypot(x - last.x, y - last.y);
+    // }
 
-// section4.onmousemove = (e) => {
-//   if (distanceFromLast(e.clientX, e.clientY) > 300) {
-//     const lead = reviews[globalIndex % reviews.length];
-//     tail = reviews[(globalIndex - 3) % reviews.length];
+    // window.onmousemove = e => {
+    //   if(distanceFromLast(e.clientX, e.clientY) > 300) {
+    //     const lead = reviews[globalIndex % reviews.length];
+    //           tail = reviews[(globalIndex - 3) % reviews.length];
 
-//     activate(lead, e.clientX, e.clientY);
+    //     activate(lead, e.clientX, e.clientY);
 
-//     if (tail) tail.dataset.status = "inactive";
+    //     if(tail) tail.dataset.status = "inactive";
 
-//     globalIndex++;
-//   }
-// };
-
-
-
-    const reviews = document.getElementsByClassName("review");
-
-    let globalIndex = 0;
-      last = { x: 0, y: 0};
-
-    const activate = (review, x, y) => {
-      review.style.left = `${x}px`;
-      review.style.top = `${y}px`;
-
-      review.dataset.status = 'active';
-
-      last = { x, y };
-    }
-
-    const distanceFromLast = (x, y) => {
-      return Math.hypot(x - last.x, y - last.y);
-    }
-
-    window.onmousemove = e => {
-      if(distanceFromLast(e.clientX, e.clientY) > 300) {
-        const lead = reviews[globalIndex % reviews.length];
-              tail = reviews[(globalIndex - 3) % reviews.length];
-
-        activate(lead, e.clientX, e.clientY);
-
-        if(tail) tail.dataset.status = "inactive";
-
-        globalIndex++;
-      }
-    }
+    //     globalIndex++;
+    //   }
+    // }
 
 
 //   }
