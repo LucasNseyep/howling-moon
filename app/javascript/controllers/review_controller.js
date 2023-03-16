2f883b0a76e173919130d3973b040cd85f91fcef
arman akther <armanakther@live.co.uk>
2023-03-16 10:22:18 +0000

    Changes from pitch

--- app/javascript/controllers/review_controller.js
+++ app/javascript/controllers/review_controller.js
@@ -2,38 +2,14 @@
 
 // export default class extends Controller {
 
-//   connect() {
+  // connect() {
 
-    // const reviews = document.getElementsByClassName("review");
-
-    // let globalIndex = 0;
-    //   last = { x: 0, y: 0};
-
-    // const activate = (review, x, y) => {
-    //   review.style.left = `${x}px`;
-    //   review.style.top = `${y}px`;
-
-    //   review.dataset.status = 'active';
-
-    //   last = { x, y };
-    // }
-
-    // const distanceFromLast = (x, y) => {
-    //   return Math.hypot(x - last.x, y - last.y);
-    // }
-
-    // window.onmousemove = e => {
-    //   if(distanceFromLast(e.clientX, e.clientY) > 300) {
-    //     const lead = reviews[globalIndex % reviews.length];
-    //           tail = reviews[(globalIndex - 3) % reviews.length];
-
-    //     activate(lead, e.clientX, e.clientY);
-
-    //     if(tail) tail.dataset.status = "inactive";
-
-    //     globalIndex++;
-    //   }
-    // }
+    $(function() {
+      $('.scroll-down').click (function() {
+        $('html, body').animate({scrollTop: $('section.section2').offset().top }, 'slow');
+        return false;
+      });
+    });
 
 
 //   }
