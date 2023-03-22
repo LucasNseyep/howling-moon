751c046931d7b275042c8accb00c16dd00e2d3df
Lucas Nseyep <lucas.nseyep@gmail.com>
2023-03-22 21:25:11 +0000

    Tests for Thought and User models

--- test/models/thought_test.rb
+++ test/models/thought_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ThoughtTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save thought if title and content are empty" do
+    thought = Thought.new
+    assert_not thought.save, "Saved empty thought"
+  end
+
+  test "Should save thought with only the content" do
+    thought = Thought.new
+    thought.user = User.first
+    thought.content = "Thought Content"
+    assert thought.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without username" do
+    user = User.new
+    user.email = "a@abc.com"
+    user.password = "1234567"
+    assert_not user.save, "Saved the user without a username"
+  end
+
+  test "should save user with email, password and username" do
+    user = User.new
+    user.email = "a@abc.com"
+    user.password = "1234567"
+    user.username = "Abi"
+    assert user.save
+  end
 end
